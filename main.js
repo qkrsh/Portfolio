0be987accc932a39437bd8df3f6222c3704dcd34
qkrsh <qkrtkdgus@gachon.ac.kr>
2022-03-18 10:06:27 +0900

    JS muddy home part

--- main.js
+++ main.js
@@ -36,3 +36,16 @@ contact.addEventListener('click',()=>{
     scroll.scrollIntoView({behavior:'smooth'});     
 }
 )
+
+//when scroll muddy home part
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', ()=>{
+       
+    home.style.opacity=1-window.scrollY/homeHeight
+    
+}
+
+)
